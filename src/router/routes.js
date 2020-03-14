f9b84aed6717f2436b0abd5f980193783dc682d9
eelke <eelkevanderbij.prive+github@gmail.com>
2020-03-14 09:49:49 -0300

    Add route for ExampleScrollPage

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,13 +1,20 @@
 import HomePage from '../page/HomePage';
+import ExampleScrollPage from '../page/ExampleScrollPage';
 
 export const RouteNames = {
   HOME: 'home',
+  EXAMPLE: 'example',
 };
 
 export default [
   {
-    path: '/',
+    path: '/home',
     component: HomePage,
     name: RouteNames.HOME,
+  },
+  {
+    path: '/',
+    component: ExampleScrollPage,
+    name: RouteNames.EXAMPLE,
   },
 ];
