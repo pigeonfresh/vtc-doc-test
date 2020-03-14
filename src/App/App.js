3bace1e0b0ad255178466ea9885e3acee2a78743
eelke <eelkevanderbij.prive+github@gmail.com>
2020-03-14 09:24:32 -0300

    Install the ScrollTrackerComponentManager plugin

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,4 @@
-import { FlowManager, AbstractRegistrableComponent } from 'vue-transition-component';
+import { FlowManager, AbstractRegistrableComponent, ScrollTrackerPlugin } from 'vue-transition-component';
 import { DeviceStateEvent } from 'seng-device-state-tracker';
 import { mapMutations, mapState } from 'vuex';
 import { SET_DEVICE_STATE } from '../store/module/app/app';
@@ -11,6 +11,19 @@ export default {
     ...mapState({
       deviceState: state => state.app.deviceState,
     }),
+  },
+  mounted() {
+    Vue.use(ScrollTrackerPlugin, {
+      config: {
+        // When this is enabled you should set the container(body) to a fixed height(100%).
+        enableSmoothScroll: false,
+        // When this is set to a container other than the window, you need to set
+        // the html/body tag to a fixed height(100%) and overflow: hidden.
+        // And set the container to a fixed height(100%) and overflow: auto.
+        container: this.$refs.container,
+        inViewProgressEnabled: false,
+      },
+    });
   },
   created() {
     this.$deviceStateTracker.addEventListener(
